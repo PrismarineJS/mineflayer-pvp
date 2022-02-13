039b06c7680ca77ec149dbde7441b2453cb34b15
Robert Schuh <github@eneticum.de>
2022-02-13 21:08:13 +0100

    fix types for pvp field on bot (#45)
    * fix types for pvp field on bot
    
    * remove unused directive

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,13 @@ import { PVP } from "./PVP";
 export function plugin(bot: Bot)
 {
     const pvp = new PVP(bot);
-
-    // @ts-expect-error
     bot.pvp = pvp;
+}
+
+declare module "mineflayer" {
+    interface Bot {
+        pvp: PVP;
+    }
 }
 
 export * from './Cooldown';
